b3a06c39794f87802c4f45510dee7b2827033a6c
Rafal Zdeb <rafal.zdeb@gmail.com>
2022-02-21 16:21:38 +0100

    get available links array

--- noSpoon.ts
+++ noSpoon.ts
@@ -1,4 +1,4 @@
-/**
+ /**
  * The machines are gaining ground. Time to show them what we're really made of...
  **/
 //  var readline = require('readline');
@@ -42,25 +42,6 @@ class Cell {
     }
 }
 
-
-// class MyNode {
-//     coordinates: Point;
-//     value: number;
-//     activeLinks: number;
-//     nodeToTheRight: Point;
-//     nodeToTheBottom: Point;
-
-//     constructor(point: Point){
-//         this.coordinates = point;
-//         this.value = Number(point.getPointValue());
-//         this.activeLinks = 0;
-//         this.nodeToTheRight = point.findRightNode();
-//         this.nodeToTheBottom = point.findDownNode()
-//     }
-// }
-
-// let nodesState: MyNode[] = [];
-
 class Link{
    node1: Point;
    node2: Point;
@@ -72,12 +53,8 @@ class Link{
        this.numberOfLinks = 0;
    }
 }
-
-  
-let linksArray:Link[] = [];
-
  
-
+let linksArray:Link[] = [];
 
 class Point{
    x: number;
@@ -88,63 +65,10 @@ class Point{
        this.y = y;
    }
 
-//    getPointValue():string{
-//        if(this.x <= width - 1 && this.y <= height - 1){
-//            const position = this.y * width + this.x + 1;
-//            return position === data.length ? data.substring(position - 1) : data.substring(position - 1, position)
-//        } else {
-//            return "."
-//        }
-//    }
-
-//    isNode(): Boolean{
-//        if (this.getPointValue().toString() === ".") return false
-//        else return true
-//    }
-
-    // findRightNode(): Point {
-    //     let result = new Point(-1, -1)
-    //     for (let i = this.x + 1; i <= width; i++){
-    //         const p = new Point (i, this.y)
-    //         if (p.isNode()) {
-    //             console.error("###");
-    //             console.error("finding right node")
-    //             console.error(this.x + ":" + this.y)
-    //             console.error(p);
-    //             console.error(p.getPointValue().toString());
-    //             console.error("###");
-    //             result = p
-    //             break
-    //         }
-    //     }
-    //     return result;
-    // }
-
-    // findDownNode(): Point {
-    //     let result = new Point(-1, -1)
-    //     for (let i = this.y + 1; i <= height; i++){
-    //         const p = new Point (this.x, i);
-    //         if(p.isNode()) {
-    //             result = p
-    //             break
-    //         }
-    //     }
-    //     return result
-    // }
-
     isValid(): Boolean {
         if(this.x === -1 && this.y === -1) return false
         else return true
     }
-
-    // isRightNode(): Boolean {
-    //     const testNode = this.findRightNode()
-    //     if (testNode.toString() === "-1 -1"){
-    //         return false
-    //     } else {
-    //         return true
-    //     }
-    // }
 
     toString(){
         return `${this.x} ${this.y}`
@@ -169,111 +93,108 @@ const cells = ():Cell[] => {
 
 const board = {
    
-   fields: cells(),
+    fields: cells(),
    
-   showAllFields(){
+    showAllFields(){
        this.fields.forEach(element => {
            console.error(element)
        });
-   },
+    },
 
-   getField(pos:Point):Cell{
-    for (const f of this.fields){
-        if (f.position.x === pos.x && f.position.y === pos.y) return f
-    }
-},
+    getField(pos:Point):Cell{
+        for (const f of this.fields){
+            if (f.position.x === pos.x && f.position.y === pos.y) return f
+        }
+    },
 
-  
+    getNearbyCells(field:Cell):Cell[] {
+        let result: Cell[] = [];
+        
+        for (const dir in Direction){
+            const d = this.findNearbyCell(field, Direction[dir])
+            if (d !== "") result.push(d)
+        }
+        return result;
+    },
 
-  
-
-
-
-   getNearbyCells(field:Cell):Cell[] {
-    let result: Cell[] = [];
-    
-    for (const dir in Direction){
-        const d = this.findNearbyCell(field, Direction[dir])
-        if (d !== "") result.push(d)
-    }
-    return result;
-},
-
-   findNearbyCell(cell:Cell, dir: Direction): Cell | string {
-            let pos = cell.position
-            while (this.isWithinBoard(pos)){
-                switch (dir) {
-                    case Direction.Up: pos = this.moveUp(pos); break;
-                    case Direction.Down: pos = this.moveDown(pos); break;
-                    case Direction.Left: pos = this.moveLeft(pos); break;
-                    case Direction.Right: pos = this.moveRight(pos); break;
-                }
+    findNearbyCell(cell:Cell, dir: Direction): Cell | string {
+        let pos = cell.position
+        while (this.isWithinBoard(pos)){
+            switch (dir) {
+                case Direction.Up: pos = this.moveUp(pos); break;
+                case Direction.Down: pos = this.moveDown(pos); break;
+                case Direction.Left: pos = this.moveLeft(pos); break;
+                case Direction.Right: pos = this.moveRight(pos); break;
+            }
+            if (this.isWithinBoard(pos)){
                 const n = board.getField(pos)
                 if (n.isNode) return n
-            }
-            return "";
+            } 
+            else {return "";}
+        }
+        return "";
     },
 
     isWithinBoard(pos:Point):boolean{
         let result = false
         for (const f of this.fields){
-         if (f.position.x === pos.x && f.position.y === pos.y) {
-             result = result || true
-         }
-         else {
-             result = result || false
-         }   
+            if (f.position.x === pos.x && f.position.y === pos.y) {
+                result = result || true
+            }
+            else {
+                result = result || false
+            }   
         }
         return result;
     },
   
-   moveRight(pos: Point): Point{return new Point(pos.x + 1, pos.y)},
-   moveLeft(pos: Point): Point{return new Point(pos.x - 1, pos.y)},
-   moveUp(pos: Point): Point{return new Point(pos.x, pos.y - 1)},
-   moveDown(pos: Point): Point{return new Point(pos.x, pos.y + 1)},
+    moveRight(pos: Point): Point{return new Point(pos.x + 1, pos.y)},
+    moveLeft(pos: Point): Point{return new Point(pos.x - 1, pos.y)},
+    moveUp(pos: Point): Point{return new Point(pos.x, pos.y - 1)},
+    moveDown(pos: Point): Point{return new Point(pos.x, pos.y + 1)},
 
-   showPossibleLinks(){
+    
 
-},
+    getPossibleLinks(){
+        let possibleLinks :Array<[Cell,Cell]> = [];
+        board.fields.map(element => {
+            if(element.isNode){
+                board.getNearbyCells(element).forEach(cell => {
+                    if (!checkCellsDuplicate(element, cell, possibleLinks)){
+                        possibleLinks.push([element, cell])
+                    }
+                })
+            }
+        });
+        
+        return possibleLinks;
+        
+        
+        // possibleLinks.forEach(c => {
+        //     console.error(c[0].position + " : " + c[1].position)
+        // })
 
-   
+        function checkCellsDuplicate(cell1:Cell , cell2:Cell, array:Array<[Cell,Cell]>): boolean{
+            const candidate = [cell2, cell1];
+            let result = false;
+            array.forEach(pair => {
+                if (JSON.stringify(candidate) === JSON.stringify(pair)) {
+                    result = result || true
+                } else {
+                    result = result || false
+                }    
+            })
+            return result
+        }
+    }  
 }
-
-
-
-
-
-// function isNode (position: Point): Boolean{
-//     return position.getPointValue.toString() !== "." ? true : false;
-// }
 
 // Write an action using console.log()
 // To debug: console.error('Debug messages...');
 
 
 // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
-
-
-
-// function findNodes() {
-//    for (const index in data) {
-//        const currDataValue = data[index]
-//        if (currDataValue !== ".") {
-//            const currPoint = pos2Point(+index);
-//            console.error(index);
-//            console.error(currDataValue);
-//            console.error(currPoint);
-//            console.error(currPoint.isNode());
-//            console.error("-----------------");
-   
-//            if (currPoint.isNode()) {
-//                const node = new MyNode(currPoint);
-//                //node.value = parseInt(currPosition);
-//                if (!nodesState.includes(node)) nodesState.push(node);
-//            }
-//        }
-//    }
-// }
+ 
 
 // function findAllLinks(){
 //     nodesState.forEach((node)=> {
@@ -328,15 +249,8 @@ const board = {
 // number of links must match the node number (check when linking)
 // all nodes connect into one group
 
-board.showAllFields();
-const testCell = new Point(2, 0);
+const possibleLinks = board.getPossibleLinks();
+console.error("^^^^^^^^^")
 
-console.error("test getField  should be 2 " + board.getField(testCell));
-console.error("--------")
-console.error(board.getNearbyCells(board.getField(testCell)));
-
-for (const en in Direction) {
-    console.error(" ENUM " + Direction[en])
-}
 
 console.log('0 0 2 0 1');
